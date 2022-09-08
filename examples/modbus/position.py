2d462b2800588106edcf829d76dc175eab9e45de
lrsch <elias.rosch@festo.com>
2022-09-08 16:09:57 +0200

    Updated naming of example variables

--- examples/modbus/position.py
+++ examples/modbus/position.py
@@ -2,11 +2,11 @@ from edrive.edrive_modbus import EDriveModbus
 from edrive.edrive_motion import EDriveMotion
 
 edrive = EDriveModbus('192.168.0.51')
-with EDriveMotion(edrive) as pos:
-    pos.acknowledge_faults()
-    pos.enable_powerstage()
-    pos.referencing_task()
+with EDriveMotion(edrive) as mot:
+    mot.acknowledge_faults()
+    mot.enable_powerstage()
+    mot.referencing_task()
 
-    pos.position_task(10000, 600000)
-    pos.position_task(-5000, 50000)
-    pos.position_task(30000, 600000, absolute=True)
+    mot.position_task(10000, 600000)
+    mot.position_task(-5000, 50000)
+    mot.position_task(30000, 600000, absolute=True)
--- examples/modbus/velocity_nonblock.py
+++ examples/modbus/velocity_nonblock.py
@@ -3,21 +3,21 @@ from edrive.edrive_modbus import EDriveModbus
 from edrive.edrive_motion import EDriveMotion
 
 edrive = EDriveModbus('192.168.0.51')
-with EDriveMotion(edrive) as pos:
-    pos.acknowledge_faults()
-    pos.enable_powerstage()
-    pos.referencing_task()
+with EDriveMotion(edrive) as mot:
+    mot.acknowledge_faults()
+    mot.enable_powerstage()
+    mot.referencing_task()
 
     # Enable continuous update so that new position tasks can be started while still in motion
-    pos.configure_continuous_update(True)
+    mot.configure_continuous_update(True)
 
-    pos.velocity_task(500000)
+    mot.velocity_task(500000)
     time.sleep(1)
-    pos.velocity_task(-100000)
+    mot.velocity_task(-100000)
     time.sleep(1)
     # Be aware that only changing the absolute velocity value update the ongoing task
-    pos.velocity_task(100001)
+    mot.velocity_task(100001)
 
     print("Wait for some time ...", end='')
-    time.sleep(3)
+    mot.wait_for_duration(3)
     print("done!")
