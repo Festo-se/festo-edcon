e6bb3684002c8009646d851f1fae9017f7bea8d6
Tobias Kreuzer <tobias.kreuzer@festo.com>
2023-12-08 11:23:55 +0100

    Menu bar replaced with tabs

--- src/edcon/gui/mainwindow.py
+++ src/edcon/gui/mainwindow.py
@@ -1,44 +1,35 @@
-# mainwindow
-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QStackedWidget, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTabWidget, QWidget
 from PyQt5.uic import loadUi
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("C:/Workspace/festo-edcon/src/edcon/gui/mainwindow.ui", self)
-        self.setWindowTitle("MainWindow")
+        self.setWindowTitle("GUI")
 
-        # Create a stacked widget to hold the different pages
-        self.stackedWidget = QStackedWidget()
-        self.setCentralWidget(self.stackedWidget)
+        # Create a tab widget to hold the different pages
+        self.tabWidget = QTabWidget()
+        self.setCentralWidget(self.tabWidget)
 
         # Create the connection page
-        connection_page = QMainWindow()
+        connection_page = QWidget()
         loadUi("C:/Workspace/festo-edcon/src/edcon/gui/connection.ui", connection_page)
-        self.stackedWidget.addWidget(connection_page)
+        self.tabWidget.addTab(connection_page, "Connection")
 
         # Create the configuration page
-        configuration_page = QMainWindow()
+        configuration_page = QWidget()
         loadUi("C:/Workspace/festo-edcon/src/edcon/gui/configuration.ui", configuration_page)
-        self.stackedWidget.addWidget(configuration_page)
+        self.tabWidget.addTab(configuration_page, "Configuration")
 
         # Create the movement page
-        movement_page = QMainWindow()
+        movement_page = QWidget()
         loadUi("C:/Workspace/festo-edcon/src/edcon/gui/movement.ui", movement_page)
-        self.stackedWidget.addWidget(movement_page)
+        self.tabWidget.addTab(movement_page, "Movement")
 
         # Create the analysis page
-        analysis_page = QMainWindow()
+        analysis_page = QWidget()
         loadUi("C:/Workspace/festo-edcon/src/edcon/gui/analysis.ui", analysis_page)
-        self.stackedWidget.addWidget(analysis_page)
-
-        # Connect the menu actions to switch between pages
-        self.actionconnection.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(0))
-        self.actionconfiguration.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(1))
-        self.actionmovement.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(2))
-        self.actionanalysis.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(3))
-
+        self.tabWidget.addTab(analysis_page, "Analysis")
 
 if __name__ == "__main__":
     app = QApplication([])
