83067e88f605b80e6c5c0d186ff7bdd62e494677
lrsch <elias.rosch@festo.com>
2023-12-12 16:53:32 +0100

    Added conditional/optional dependencies.

--- src/edcon/cli/gui.py
+++ src/edcon/cli/gui.py
@@ -1,7 +1,5 @@
 """CLI tool starts the gui"""
-from PyQt5.QtWidgets import QApplication
-from edcon.gui.mainwindow import MainWindow
-
+from edcon.utils.logging import Logging
 def add_gui_parser(subparsers):
     """Adds arguments to a provided subparsers instance"""
     parser_gui = subparsers.add_parser('gui')
@@ -10,6 +8,16 @@ def add_gui_parser(subparsers):
 
 def gui_func(args):
     """Executes subcommand based on provided arguments"""
+    try:
+        from PyQt5.QtWidgets import QApplication
+        from edcon.gui.mainwindow import MainWindow
+    except ImportError:
+        Logging.logger.error("GUI dependency PyQt5 could not be imported.\n"
+                             "Possible remedies:\n"
+                             "1. Install festo-edcon with GUI support: 'pip install festo-edcon[gui]'\n"
+                             "2. Install PyQt5 manually: 'pip install pyqt5'\n")
+        return
+
     app = QApplication([])
     window = MainWindow()
     window.show()
