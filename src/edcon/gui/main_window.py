9fc27847eaa0e2dfba4ccaa61e520e983eea0f8b
Tobias Kreuzer <tobias.kreuzer@festo.com>
2023-12-15 15:25:29 +0100

    Pylint test conducted and fixed

--- src/edcon/gui/main_window.py
+++ src/edcon/gui/main_window.py
@@ -18,79 +18,86 @@ class MainWindow(QMainWindow):
 
         self.connection_widget = loadUi(
             PurePath(files('edcon') / 'gui' / 'connection.ui'))
-        self.connection_widget.txteditIP.setText(ip_address)
+        self.connection_widget.line_edit_ip.setText(ip_address)
         self.toolBar.addWidget(self.connection_widget)
 
         # # Create an instance of CustomConnection
         self.connection_widget = ConnectionWidget(
-            self.connection_widget.txteditIP, self.connection_widget.btnConnect)
+            self.connection_widget.line_edit_ip, self.connection_widget.btnConnect)
 
         # Create an instance of ParameterTableModel
         csv_file_path = PurePath(
             files('edcon') / 'edrive' / 'data' / 'pnu_map.csv')
         self.model = ParameterTableModel(csv_file_path, self.connection_widget)
 
-        self.btnUpdate.clicked.connect(self.btn_update_clicked)
-        self.btnConfirmValue.clicked.connect(self.btn_confirm_clicked)
-        self.btnReadValue.clicked.connect(self.btn_read_clicked)
-        self.tblPnuList.setModel(self.model)
-        self.txtsearchPnuList.textChanged.connect(self.on_text_changed)
-        self.txtValueParameter.returnPressed.connect(self.pressed_enter_value)
-        self.txtsearchParameter.returnPressed.connect(self.pressed_enter_read)
+        self.btn_update.clicked.connect(self.btn_update_clicked)
+        self.btn_confirm.clicked.connect(self.btn_confirm_clicked)
+        self.btn_read.clicked.connect(self.btn_read_clicked)
+        self.tbl_pnu_list.setModel(self.model)
+        self.line_edit_pnu_list.textChanged.connect(self.on_text_changed)
+        self.line_edit_pnu_value.returnPressed.connect(self.pressed_enter_value)
+        self.line_edit_pnu.returnPressed.connect(self.pressed_enter_read)
 
     def read_pnu_from_text(self):
         """Takes input from line edit and reads corresponding PNU."""
         try:
             # Convert pnu to an integer
-            pnu = int(self.txtsearchParameter.text())
+            pnu = int(self.line_edit_pnu.text())
         except ValueError as exception:
             QMessageBox.warning(
-                self.txtValueParameter.parent(),
+                self.line_edit_pnu_value.parent(),
                 "Read PNU Failed",
-                f"Failed to read PNU{self.txtsearchParameter.text()}: {str(exception)}"
+                f"Failed to read PNU{self.line_edit_pnu.text()}: {str(exception)}"
             )
             return
 
         result = self.connection_widget.com.read_pnu(pnu, 0)
         # Do something with the result, such as displaying it in a QMessageBox
-        QMessageBox.information(self.txtsearchParameter.parent(
+        QMessageBox.information(self.line_edit_pnu.parent(
         ), "Read PNU Result", f"The result for PNU {pnu} is: {result}")
 
     def write_pnu_value_from_text(self):
-        new_value = self.txtValueParameter.text()
+        """Takes input from line edit and write value into corresponding PNU."""
+        new_value = self.line_edit_pnu_value.text()
         try:
             # Convert pnu to an integer
-            pnu = int(self.txtsearchParameter.text())
+            pnu = int(self.line_edit_pnu.text())
         except ValueError as exception:
             QMessageBox.warning(
-                self.txtValueParameter.parent(),
+                self.line_edit_pnu_value.parent(),
                 "Write PNU Failed",
-                f"Failed to write PNU{self.txtsearchParameter.text()}: {str(exception)}"
+                f"Failed to write PNU{self.line_edit_pnu.text()}: {str(exception)}"
             )
             return
 
         self.connection_widget.com.write_pnu(pnu, 0, new_value)
         # Do something with the result, such as displaying it in a QMessageBox
-        QMessageBox.information(self.txtValueParameter.parent(
+        QMessageBox.information(self.line_edit_pnu_value.parent(
         ), "Write PNU successfull", f"The value {new_value} is written in: {pnu}")
 
     def on_text_changed(self, text):
+        """Searches for the content of the line edit in the table."""
         self.model.set_name_filter(text)
-        self.tblPnuList.model().layoutChanged.emit()
+        self.tbl_pnu_list.model().layoutChanged.emit()
 
     def btn_update_clicked(self):
+        """Activate the function set_pnu_for_all_rows."""
         self.model.set_pnu_for_all_rows()
 
     def btn_confirm_clicked(self):
+        """Activate the functions set_value_for_one_row and write_pnu_value_from_text."""
         self.write_pnu_value_from_text()
-        self.model.set_value_for_one_row(self.txtsearchParameter.text())
+        self.model.set_value_for_one_row(self.line_edit_pnu.text())
 
     def btn_read_clicked(self):
+        """Activate the function read_pnu_from_text."""
         self.read_pnu_from_text()
 
     def pressed_enter_read(self):
+        """Activate the function read_pnu_from_text."""
         self.read_pnu_from_text()
 
     def pressed_enter_value(self):
+        """Activate the function write_pnu_value_from_text and set_value_for_one_row."""
         self.write_pnu_value_from_text()
-        self.model.set_value_for_one_row(self.txtsearchParameter.text())
+        self.model.set_value_for_one_row(self.line_edit_pnu.text())
--- src/edcon/gui/parameter_table_model.py
+++ src/edcon/gui/parameter_table_model.py
@@ -20,6 +20,7 @@ class ParameterTableModel(QtCore.QAbstractTableModel):
             self._filtered_data = self._data
 
     def set_name_filter(self, name_filter):
+        """takes input from line edit and filter table widget"""
         self.name_filter = name_filter
         if name_filter == '':
             self._filtered_data = self._data
@@ -27,11 +28,13 @@ class ParameterTableModel(QtCore.QAbstractTableModel):
             line for line in self._data if name_filter in line[1]]
 
     def set_pnu_for_all_rows(self):
+        """Fill the column "value" for all rows."""
         pnu_list = [row[0] for row in self._data]
         for pnu in pnu_list:
             self.set_value_for_one_row(pnu)
 
     def set_value_for_one_row(self, pnu):
+        """Fill the column "value" for one row."""
         try:
             # Execute the command with the current pnu value
             value = self.connection_widget.com.read_pnu(int(pnu), 0)
@@ -42,6 +45,7 @@ class ParameterTableModel(QtCore.QAbstractTableModel):
             pass
 
     def data(self, index, role):
+        """returns the cell value if the role is "Qt.DisplayRole"""
         if role == Qt.DisplayRole:
             row = index.row()
             column = index.column()
@@ -51,16 +55,19 @@ class ParameterTableModel(QtCore.QAbstractTableModel):
     # pylint: disable=invalid-name, unused-argument
     # PyQt API naming
     def rowCount(self, index):
+        """returns the number of rows."""
         return len(self._filtered_data)
 
     # pylint: disable=invalid-name, unused-argument
     # PyQt API naming
     def columnCount(self, index):
+        """returns the number of columns."""
         return len(self._headers)
 
     # pylint: disable=invalid-name
     # PyQt API naming
     def headerData(self, section, orientation, role):
+        """Used to provide data for the header rows"""
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self._headers[section]
         if orientation == Qt.Vertical and role == Qt.DisplayRole:
