032196ecf686a6e6b7d7706fb3bbcedb8bf1d289
lrsch <elias.rosch@festo.com>
2024-04-17 16:46:29 +0200

    Updated unit test.

--- tests/edrive/test_motion_handler.py
+++ tests/edrive/test_motion_handler.py
@@ -1,4 +1,5 @@
 """Contains tests for MotionHandler class"""
+
 from edcon.edrive.motion_handler import MotionHandler
 from unittest.mock import Mock, MagicMock
 from edcon.profidrive.words import NIST_B
@@ -8,7 +9,7 @@ class TestMotionHandler:
     def test_over_v_set(self):
         """Tests over_v property"""
         com = Mock()
-        mot = MotionHandler(com, skip_validation=True)
+        mot = MotionHandler(com)
         assert mot.telegram.override.value == 16384
         mot.over_v = 50.0
         assert mot.telegram.override.value == 8192
@@ -16,17 +17,17 @@ class TestMotionHandler:
     def test_over_v_get(self):
         """Tests over_v property"""
         com = Mock()
-        mot = MotionHandler(com, skip_validation=True)
+        mot = MotionHandler(com)
         mot.telegram.override.value = 8192
         assert mot.over_v == 50.0
 
     def test_over_v_set_multiple_instances(self):
         """Tests over_v property"""
         com = Mock()
-        mot = MotionHandler(com, skip_validation=True)
+        mot = MotionHandler(com)
         assert mot.telegram.override.value == 16384
 
-        mot2 = MotionHandler(com, skip_validation=True)
+        mot2 = MotionHandler(com)
         assert mot2.telegram.override.value == 16384
 
         mot.over_v = 50.0
@@ -37,7 +38,7 @@ class TestMotionHandler:
     def test_over_acc(self):
         """Tests over_acc property"""
         com = Mock()
-        mot = MotionHandler(com, skip_validation=True)
+        mot = MotionHandler(com)
         assert mot.telegram.mdi_acc.value == 16384
         mot.over_acc = 50.0
         assert mot.telegram.mdi_acc.value == 8192
@@ -45,7 +46,7 @@ class TestMotionHandler:
     def test_over_dec(self):
         """Tests over_dec property"""
         com = Mock()
-        mot = MotionHandler(com, skip_validation=True)
+        mot = MotionHandler(com)
         assert mot.telegram.mdi_dec.value == 16384
         mot.over_dec = 50.0
         assert mot.telegram.mdi_dec.value == 8192
@@ -53,7 +54,7 @@ class TestMotionHandler:
     def test_current_velocity(self):
         """Tests current_velocity"""
         com = Mock()
-        mot = MotionHandler(com, skip_validation=True)
+        mot = MotionHandler(com)
         mot.update_inputs = Mock()
 
         mot.base_velocity = 1000
