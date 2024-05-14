1ef52aa2d538ff4a7bc26783f32eb158a89e89aa
Tobias Kreuzer <tobias.kreuzer@festo.com>
2024-05-14 14:35:38 +0200

    branch for motion-tab

--- src/edcon/gui/main_window.py
+++ src/edcon/gui/main_window.py
@@ -9,6 +9,7 @@ from PyQt5.uic import loadUi
 from edcon.gui.connection_widget import ConnectionWidget
 from edcon.gui.parameter_tab import ParameterTab
 from edcon.gui.processdata_tab import ProcessDataTab
+from edcon.gui.motion_tab import MotionTab
 from edcon.edrive.com_modbus import ComModbus
 
 
@@ -28,12 +29,12 @@ class MainWindow(QMainWindow):
 
         self.toolBar.addWidget(self.connection_widget)
 
+        self.motion_tab = MotionTab(self.get_com_function)
+        self.tabWidget.addTab(self.motion_tab, "Motion")
+        self.tabWidget.addTab(ProcessDataTab(self.get_com_function), "Process data")
         self.tabWidget.addTab(
             ParameterTab(self.pnu_read_function, self.pnu_write_function), "Parameter"
         )
-
-        self.tabWidget.addTab(ProcessDataTab(self.get_com_function), "Process data")
-
     @property
     def com(self):
         """
