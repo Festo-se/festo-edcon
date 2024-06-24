daddb2b3d403177a2001c5d5f1208d5d31cf646a
Tobias Kreuzer <tobias.kreuzer@festo.com>
2024-06-24 16:18:04 +0200

    dynamic state diagram with pyqtgraph

--- src/edcon/gui/state_diagram_widget.py
+++ src/edcon/gui/state_diagram_widget.py
@@ -4,7 +4,6 @@ from pathlib import PurePath
 from importlib.resources import files
 
 # pylint: disable=import-error, no-name-in-module
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
 from edcon.gui.state_diagram import StateDiagram
@@ -20,13 +19,9 @@ class StateDiagramWidget(QWidget):
             PurePath(files("edcon") / "gui" / "ui" / "state_diagram.ui"),
             self,
         )
-        self.scene = StateDiagram()
-        self.graphicsView.setVisible(True)
-        self.graphicsView.setScene(self.scene)
-        self.graphicsView.setSceneRect(self.scene.sceneRect())
-        self.graphicsView.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
-        self.graphicsView.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.view = StateDiagram()
+        self.verticalLayout.addWidget(self.view)
 
     def update(self, current_state):
         """Updates the state diagram view"""
-        self.scene.update(current_state)
+        self.view.update(current_state)
