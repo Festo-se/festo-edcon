fd4f6ecabda34b32a2526fb35e2505b0244b8cb7
Tobias Kreuzer <tobias.kreuzer@festo.com>
2024-06-24 16:18:04 +0200

    dynamic state diagram with pyqtgraph

--- src/edcon/gui/state_diagram.py
+++ src/edcon/gui/state_diagram.py
@@ -1,160 +1,131 @@
 """Scene for the state diagram."""
 
-# pylint: disable=import-error, no-name-in-module
-from PyQt5.QtWidgets import (
-    QGraphicsScene,
-    QGraphicsRectItem,
-    QGraphicsTextItem,
-    QGraphicsPathItem,
-)
-from PyQt5.QtCore import QRectF, QPointF
-from PyQt5.QtGui import QPen, QBrush, QColor, QPainterPath
-
-# Constants for dimensions and colors
-SCENE_WIDTH, SCENE_HEIGHT = 400, 775
-STATE_WIDTH, STATE_HEIGHT = 200, 80
-STATE_COLOR = "white"
-TEXT_OFFSET_X, TEXT_OFFSET_Y = 100, 40
-ARROW_COLOR = "black"
-ARROW_WIDTH = 2
-ARROW_HEAD_WIDTH, ARROW_HEAD_HEIGHT = 8, 5
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QGraphicsRectItem
+from PyQt5.QtGui import QColor, QPen, QBrush
 
 
-class StateDiagram(QGraphicsScene):
-    """Defines the state diagram scene."""
-
+class StateDiagram(pg.GraphicsLayoutWidget):
     def __init__(
         self,
     ):
         super().__init__()
-        self.setSceneRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT)
-        self.setup_states()
-        self.setup_arrows()
+        self.view = self.addViewBox()
+        self.view.setAspectLocked()
+        self.view.setBackgroundColor("w")
+        self.rects = {}
+        self.texts = {}
+        self.current_state = None
+        self.create_states()
+        self.create_arrows()
+        self.resizeEvent = self.on_resize
 
-    def setup_states(self):
+    def create_states(self):
         """Sets up and adds labeled state rectangles to the scene at specified positions"""
-        self.states = []
-        vertical_spacing = 150
-        for i in range(4):
-            label = [
-                "S1: Switching On Inhibited",
-                "S2: Ready For Switching On",
-                "S3: Switched On",
-                "S4: Operation",
-            ][i]
+        self.states = {
+            "S1": {"label": "S1: Switching On Inhibited", "pos": (0, 0)},
+            "S2": {"label": "S2: Ready For Switching On", "pos": (0, -150)},
+            "S3": {"label": "S3: Switched On", "pos": (0, -300)},
+            "S4": {"label": "S4: Operation", "pos": (0, -450)},
+        }
 
-            if label not in ("S52: Quick Stop", "S51: Ramp Stop"):
-                pos = QPointF(100, i * (STATE_HEIGHT + vertical_spacing))
-                state = QGraphicsRectItem(QRectF(0, 0, STATE_WIDTH, STATE_HEIGHT))
-                state.setPos(pos)
-                state.setBrush(QBrush(QColor(STATE_COLOR)))
-                state.setPen(QPen(QColor(ARROW_COLOR), ARROW_WIDTH))
-                self.states.append(state)
-                self.addItem(state)
-                text = QGraphicsTextItem(label)
-                text.setPos(
-                    pos
-                    + QPointF(
-                        STATE_WIDTH / 2 - text.boundingRect().width() / 2,
-                        STATE_HEIGHT / 2 - text.boundingRect().height() / 2,
-                    )
-                )
-                self.addItem(text)
+        for state, data in self.states.items():
+            x, y = data["pos"]
+            rect = QGraphicsRectItem(-100, -40, 200, 80)
+            rect.setPos(x, y)
+            color = QColor(200, 200, 200)
+            pen = QPen(QColor("#0056b3"))
+            pen.setWidth(2)
+            brush = QBrush(color)
+            rect.setPen(pen)
+            rect.setBrush(brush)
+            self.view.addItem(rect)
+            self.rects[state] = rect
 
-    def add_arrow(self, start, end, condition_left, condition_right):
-        """Adds arrows and their conditions on the correct place
+            text = pg.TextItem(data["label"], anchor=(0.5, 0.5), color="white")
+            text.setPos(x, y)
+            self.view.addItem(text)
+            self.texts[state] = text
 
-        Parameters:
-            start(int): starting point
-            end(int):   ending point
-            condition_left(str): condition on the left side
-            condition_right(str): condition on the right side
-            offset(int)
-        """
-        if condition_left == "":
-            offset = 15
-        else:
-            offset = -45
-        # Adjust starting and ending points based on offset for parallel arrows
-        start = QPointF(start[0] + offset, start[1])
-        end = QPointF(end[0] + offset, end[1])
+    def create_arrows(self):
+        """Setup the positions for the arrows and their conditions on the correct place"""
 
-        # Create the main line of the arrow
-        path = QPainterPath(start)
-        path.lineTo(end)
-        arrow = QGraphicsPathItem(path)
-        arrow.setPen(QPen(QColor(ARROW_COLOR), ARROW_WIDTH))
-        self.addItem(arrow)
-
-        # Create the arrow head at the end point
-        arrow_head = QPainterPath()
-        if offset > 0:
-            # Arrow head pointing downwards
-            arrow_head.moveTo(end)
-            arrow_head.lineTo(end + QPointF(-ARROW_HEAD_WIDTH, ARROW_HEAD_HEIGHT))
-            arrow_head.lineTo(end + QPointF(ARROW_HEAD_WIDTH, ARROW_HEAD_HEIGHT))
-        else:
-            # Arrow head pointing upwards
-            arrow_head.moveTo(end)
-            arrow_head.lineTo(end + QPointF(-ARROW_HEAD_WIDTH, -ARROW_HEAD_HEIGHT))
-            arrow_head.lineTo(end + QPointF(ARROW_HEAD_WIDTH, -ARROW_HEAD_HEIGHT))
-        arrow_head.closeSubpath()  # Close the path to make a solid triangle
-        arrow_item = QGraphicsPathItem(arrow_head)
-        arrow_item.setBrush(QColor(ARROW_COLOR))
-        self.addItem(arrow_item)
-
-        text_left = QGraphicsTextItem(condition_left)
-        mid_point_left = (start + end) / 2  # Calculate midpoint for text placement
-        text_left.setPos(mid_point_left + QPointF(-120, -50))  # Adjust text position
-        self.addItem(text_left)
-
-        text_right = QGraphicsTextItem(condition_right)
-        mid_point_right = (start + end) / 2  # Calculate midpoint for text placement
-        text_right.setPos(mid_point_right + QPointF(15, -50))  # Adjust text position
-        self.addItem(text_right)
-
-    def setup_arrows(self):
-        """ ""Setup the positions for the arrows"""
-        arrow_positions = [
-            (
-                (100 + STATE_WIDTH / 2, STATE_HEIGHT + 150 - ARROW_HEAD_HEIGHT),
-                (100 + STATE_WIDTH / 2, STATE_HEIGHT),
-            ),
-            (
-                (100 + STATE_WIDTH / 2, STATE_HEIGHT * 2 + 300 - ARROW_HEAD_HEIGHT),
-                (100 + STATE_WIDTH / 2, STATE_HEIGHT * 2 + 150),
-            ),
-            (
-                (100 + STATE_WIDTH / 2, STATE_HEIGHT * 3 + 450 - ARROW_HEAD_HEIGHT),
-                (100 + STATE_WIDTH / 2, STATE_HEIGHT * 3 + 300),
-            ),
+        arrows = [
+            ("S1", "S2"),
+            ("S2", "S3"),
+            ("S3", "S4"),
         ]
-        conditions_left = [
-            [
-                "OFF",
-                "AND No Coast Stop",
-                "AND No Quick Stop",
-                "STW1.0=false",
-                "STW1.1=true",
-                "STW1.2=true",
-            ],
-            ["ON", "STW1.0=true"],
-            ["Enable Operation", "STW1.3=true"],
+
+        conditions = [
+            (
+                "OFF\nAND No Coast Stop\nAND No Quick Stop\nSTW1.0=false\nSTW1.1=true\nSTW1.2=true",
+                "Coast Stop\nOR Quick Stop\nSTW1.1=false\nOR STW1.2=false",
+            ),
+            ("ON\nSTW1.0=true", "OFF\nSTW1.0=false"),
+            ("Enable Operation\nSTW1.3=true", "Disable Operation\nSTW1.3=false"),
         ]
-        conditions_right = [
-            ["Coast Stop", "OR Quick Stop", "STW1.1=false", "OR STW1.2=false"],
-            ["OFF", "STW1.0=false"],
-            ["Disable Operation", "STW1.3=false"],
-        ]
-        for (start, end), conditions_left_single, conditions_right_single in zip(
-            arrow_positions, conditions_left, conditions_right
+
+        for i, ((start, end), (condition_left, condition_right)) in enumerate(
+            zip(arrows, conditions)
         ):
-            condition_left = "\n".join(conditions_left_single)
-            condition_right = "\n".join(conditions_right_single)
-            right = ""
-            left = ""
-            self.add_arrow(start, end, left, condition_right)
-            self.add_arrow(end, start, condition_left, right)
+            start_pos = self.states[start]["pos"]
+            end_pos = self.states[end]["pos"]
+
+            # Arrow pointing down
+            line_down = pg.PlotCurveItem(
+                [start_pos[0], start_pos[0]],
+                [start_pos[1] - 40, end_pos[1] + 40],
+                pen=pg.mkPen(color=(0, 0, 0), width=2),
+            )
+            self.view.addItem(line_down)
+
+            arrow_head_down = pg.ArrowItem(
+                angle=-90, pen={"color": "k", "width": 2}, brush="k"
+            )
+            arrow_head_down.setPos(start_pos[0], end_pos[1] + 40)
+            self.view.addItem(arrow_head_down)
+
+            text_left = pg.TextItem(condition_left, anchor=(0.5, 0.5), color=(0, 0, 0))
+            text_left.setPos(
+                (start_pos[0] + end_pos[0]) / 2 - 150, (start_pos[1] + end_pos[1]) / 2
+            )
+            self.view.addItem(text_left)
+
+            # Arrow pointing up
+            line_up = pg.PlotCurveItem(
+                [end_pos[0] + 10, start_pos[0] + 10],
+                [end_pos[1] + 40, start_pos[1] - 40],
+                pen=pg.mkPen(color=(0, 0, 0), width=2),
+            )
+            self.view.addItem(line_up)
+
+            arrow_head_up = pg.ArrowItem(
+                angle=90, pen={"color": "k", "width": 2}, brush="k"
+            )
+            arrow_head_up.setPos(end_pos[0] + 10, start_pos[1] - 40)
+            self.view.addItem(arrow_head_up)
+
+            text_right = pg.TextItem(
+                condition_right, anchor=(0.5, 0.5), color=(0, 0, 0)
+            )
+            text_right.setPos(
+                (start_pos[0] + end_pos[0]) / 2 + 150, (start_pos[1] + end_pos[1]) / 2
+            )
+            self.view.addItem(text_right)
+
+    def on_resize(self, event):
+        """Handles the resize event and updates text sizes accordingly."""
+        self.update_text_sizes()
+        super().resizeEvent(event)
+
+    def update_text_sizes(self):
+        """Updates the text sizes based on the current window dimensions."""
+        width = self.width()
+        height = self.height()
+        scale_factor = min(width, height) / 900
+        for state, text in self.texts.items():
+            label_html = f'<div style="font-size: {14 * scale_factor}pt; color: white;">{self.states[state]["label"]}</div>'
+            text.setHtml(label_html)
 
     def update(self, current_state):
         """Update the active state in state diagram
@@ -162,9 +133,10 @@ class StateDiagram(QGraphicsScene):
         Parameters:
             current_state: State to be set as active
         """
+        for rect in self.rects.values():
+            rect.setBrush(pg.mkBrush(200, 200, 200))
 
-        for state in self.states:
-            state.setBrush(QBrush(QColor(STATE_COLOR)))
-
-        if 0 <= current_state < len(self.states):
-            self.states[current_state].setBrush(QBrush(QColor("light blue")))
+        state_keys = list(self.states.keys())  # This will be ["S1", "S2", "S3", "S4"]
+        if 0 <= current_state < len(state_keys):
+            state_key = state_keys[current_state]
+            self.rects[state_key].setBrush(pg.mkBrush(0, 170, 255))
