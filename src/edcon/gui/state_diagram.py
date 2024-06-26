ebe0ee3a2f31c473829a5e5a9d79f3494784b615
lrsch <elias.rosch@festo.com>
2024-06-26 09:01:14 +0200

    First cleanup state-diagram

--- src/edcon/gui/state_diagram.py
+++ src/edcon/gui/state_diagram.py
@@ -1,17 +1,27 @@
 """View for the state diagram."""
-import pyqtgraph as pg
+
+from pyqtgraph import (
+    GraphicsLayoutWidget,
+    TextItem,
+    PlotCurveItem,
+    ArrowItem,
+    mkPen,
+    mkBrush,
+)
 from PyQt5.QtWidgets import QGraphicsRectItem
 from PyQt5.QtGui import QColor, QPen, QBrush
 
 
-class StateDiagram(pg.ViewBox):
+class StateDiagram(GraphicsLayoutWidget):
     """Initializes the StateDiagram with a viewbox and sets up states and arrows."""
 
     def __init__(self):
         """Initializes the StateDiagram with a viewbox and sets up states and arrows."""
         super().__init__()
-        self.setAspectLocked()
-        self.setBackgroundColor("w")
+        self.setBackground(0.90)
+        self.view_box = self.addViewBox()
+        self.view_box.setAspectLocked()
+        self.view_box.setBackgroundColor(0.90)
         self.rects = {}
         self.texts = {}
         self.states = {
@@ -22,21 +32,17 @@ class StateDiagram(pg.ViewBox):
         }
         self.arrows = [("S1", "S2"), ("S2", "S3"), ("S3", "S4")]
         self.conditions = [
-            ("OFF\nAND No Coast Stop\nAND No Quick Stop\nSTW1.0=false\nSTW1.1=true\nSTW1.2=true", 
-             "Coast Stop\nOR Quick Stop\nSTW1.1=false\nOR STW1.2=false"),
+            (
+                "OFF\nAND No Coast Stop\nAND No Quick Stop\nSTW1.0=false\nSTW1.1=true\nSTW1.2=true",
+                "Coast Stop\nOR Quick Stop\nSTW1.1=false\nOR STW1.2=false",
+            ),
             ("ON\nSTW1.0=true", "OFF\nSTW1.0=false"),
             ("Enable Operation\nSTW1.3=true", "Disable Operation\nSTW1.3=false"),
         ]
         self.create_states()
-        self.create_arrows()
-        self.resizeEvent = self.on_resize # pylint: disable=invalid-name
+        self.create_transitions()
 
-    def create_states(self):
-        """Sets up and adds labeled state rectangles to the scene at specified positions."""
-        for state, data in self.states.items():
-            self._create_state_item(state, data["pos"], data["label"])
-
-    def _create_state_item(self, state, pos, label):
+    def create_state_item(self, state, pos, label):
         """
         Creates a state item with a rectangle and label.
 
@@ -50,20 +56,63 @@ class StateDiagram(pg.ViewBox):
         rect.setPos(x, y)
         rect.setPen(QPen(QColor("#0056b3"), 2))
         rect.setBrush(QBrush(QColor(200, 200, 200)))
-        self.addItem(rect)
+        self.view_box.addItem(rect)
         self.rects[state] = rect
 
-        text = pg.TextItem(label, anchor=(0.5, 0.5), color="white")
+        text = TextItem(label, anchor=(0.5, 0.5), color="white")
         text.setPos(x, y)
-        self.addItem(text)
+        self.view_box.addItem(text)
         self.texts[state] = text
 
-    def create_arrows(self):
-        """Sets up the positions for the arrows and their conditions at the correct places."""
-        for (start, end), (condition_left, condition_right) in zip(self.arrows, self.conditions):
-            self._create_arrow_items(start, end, condition_left, condition_right)
+    def create_states(self):
+        """Sets up and adds labeled state rectangles to the scene at specified positions."""
+        for state, data in self.states.items():
+            self.create_state_item(state, data["pos"], data["label"])
 
-    def _create_arrow_items(self, start, end, condition_left, condition_right):
+    def draw_arrow(self, start_pos, end_pos):
+        """
+        Draws a line between two positions with arrowheads.
+
+        Args:
+            start_pos (tuple): The (x, y) position of the starting point.
+            end_pos (tuple): The (x, y) position of the ending point.
+        """
+        self.view_box.addItem(
+            PlotCurveItem(
+                [start_pos[0], start_pos[0]],
+                [start_pos[1] - 40, end_pos[1] + 40],
+                pen=mkPen(color=(0, 0, 0), width=2),
+            )
+        )
+        arrow_down = ArrowItem(angle=-90, pen={"color": "k", "width": 2}, brush="k")
+        arrow_down.setPos(start_pos[0], end_pos[1] + 40)
+        self.view_box.addItem(arrow_down)
+
+        self.view_box.addItem(
+            PlotCurveItem(
+                [end_pos[0] + 10, start_pos[0] + 10],
+                [end_pos[1] + 40, start_pos[1] - 40],
+                pen=mkPen(color=(0, 0, 0), width=2),
+            )
+        )
+        arrow_up = ArrowItem(angle=90, pen={"color": "k", "width": 2}, brush="k")
+        arrow_up.setPos(end_pos[0] + 10, start_pos[1] - 40)
+        self.view_box.addItem(arrow_up)
+
+    def draw_condition(self, x, y, text_content):
+        """
+        Draws text at a specified position.
+
+        Args:
+            x (float): The x-coordinate of the text position.
+            y (float): The y-coordinate of the text position.
+            text_content (str): The content of the text.
+        """
+        text = TextItem(text_content, anchor=(0.5, 0.5), color=(0, 0, 0))
+        text.setPos(x, y)
+        self.view_box.addItem(text)
+
+    def create_transition_items(self, start, end, condition_left, condition_right):
         """
         Creates arrow items between two states with conditions.
 
@@ -75,63 +124,24 @@ class StateDiagram(pg.ViewBox):
         """
         start_pos = self.states[start]["pos"]
         end_pos = self.states[end]["pos"]
-        self._draw_arrow(start_pos, end_pos, condition_left, condition_right)
+        self.draw_arrow(start_pos, end_pos)
+        self.draw_condition(
+            (start_pos[0] + end_pos[0]) / 2 - 150,
+            (start_pos[1] + end_pos[1]) / 2,
+            condition_left,
+        )
+        self.draw_condition(
+            (start_pos[0] + end_pos[0]) / 2 + 150,
+            (start_pos[1] + end_pos[1]) / 2,
+            condition_right,
+        )
 
-    def _draw_arrow(self, start_pos, end_pos, condition_left, condition_right):
-        """
-        Draws an arrow between two positions with conditions.
-
-        Args:
-            start_pos (tuple): The (x, y) position of the starting state.
-            end_pos (tuple): The (x, y) position of the ending state.
-            condition_left (str): The condition text on the left side of the arrow.
-            condition_right (str): The condition text on the right side of the arrow.
-        """
-        self._draw_line(start_pos, end_pos)
-        self._draw_text((start_pos[0] + end_pos[0]) / 2 - 150, (start_pos[1] + end_pos[1]) / 2, condition_left)
-        self._draw_text((start_pos[0] + end_pos[0]) / 2 + 150, (start_pos[1] + end_pos[1]) / 2, condition_right)
-
-    def _draw_line(self, start_pos, end_pos):
-        """
-        Draws a line between two positions with arrowheads.
-
-        Args:
-            start_pos (tuple): The (x, y) position of the starting point.
-            end_pos (tuple): The (x, y) position of the ending point.
-        """
-        self.addItem(pg.PlotCurveItem([start_pos[0], start_pos[0]], [start_pos[1] - 40, end_pos[1] + 40], pen=pg.mkPen(color=(0, 0, 0), width=2)))
-        arrow_down = pg.ArrowItem(angle=-90, pen={"color": "k", "width": 2}, brush="k")
-        arrow_down.setPos(start_pos[0], end_pos[1] + 40)
-        self.addItem(arrow_down)
-
-        self.addItem(pg.PlotCurveItem([end_pos[0] + 10, start_pos[0] + 10], [end_pos[1] + 40, start_pos[1] - 40], pen=pg.mkPen(color=(0, 0, 0), width=2)))
-        arrow_up = pg.ArrowItem(angle=90, pen={"color": "k", "width": 2}, brush="k")
-        arrow_up.setPos(end_pos[0] + 10, start_pos[1] - 40)
-        self.addItem(arrow_up)
-
-    def _draw_text(self, x, y, text_content):
-        """
-        Draws text at a specified position.
-
-        Args:
-            x (float): The x-coordinate of the text position.
-            y (float): The y-coordinate of the text position.
-            text_content (str): The content of the text.
-        """
-        text = pg.TextItem(text_content, anchor=(0.5, 0.5), color=(0, 0, 0))
-        text.setPos(x, y)
-        self.addItem(text)
-
-    def on_resize(self, event):
-        """Handles the resize event and updates text sizes accordingly."""
-        self.update_text_sizes()
-        super().resizeEvent(event)
-
-    def update_text_sizes(self):
-        """Updates the text sizes based on the current window dimensions."""
-        scale_factor = min(self.width(), self.height()) / 900
-        for state, text in self.texts.items():
-            text.setHtml(f'<div style="font-size: {14 * scale_factor}pt; color: white;">{self.states[state]["label"]}</div>')
+    def create_transitions(self):
+        """Sets up the positions for the arrows and their conditions at the correct places."""
+        for (start, end), (condition_left, condition_right) in zip(
+            self.arrows, self.conditions
+        ):
+            self.create_transition_items(start, end, condition_left, condition_right)
 
     def update_state(self, current_state):
         """
@@ -141,7 +151,18 @@ class StateDiagram(pg.ViewBox):
             current_state (int): The index of the state to be set as active.
         """
         for rect in self.rects.values():
-            rect.setBrush(pg.mkBrush(200, 200, 200))
+            rect.setBrush(mkBrush(200, 200, 200))
         state_key = list(self.states.keys())[current_state]
-        self.rects[state_key].setBrush(pg.mkBrush(0, 170, 255))
-    
+        self.rects[state_key].setBrush(mkBrush(0, 170, 255))
+
+    # pylint: disable=invalid-name
+    # PyQt API naming
+    def resizeEvent(self, ev):
+        """Handles the resize event and updates text sizes accordingly."""
+        if ev is not None:
+            scale_factor = min(self.width(), self.height()) / 900
+            for state, text in self.texts.items():
+                text.setHtml(
+                    f'<div style="font-size: {14 * scale_factor}pt; color: white;">{self.states[state]["label"]}</div>'
+                )
+        super().resizeEvent(ev)
