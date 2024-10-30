0c99ae787354e6ff71686d6113c5e50be4b1f71d
lrsch <elias.rosch@festo.com>
2024-10-30 06:39:29 +0100

    Fixed issue due to breaking changes by pymodbus 3.7.4

--- src/edcon/edrive/com_modbus.py
+++ src/edcon/edrive/com_modbus.py
@@ -122,13 +122,13 @@ class ComModbus(ComBase):
 
         # Read device information
         rreq = ReadDeviceInformationRequest(0x1, 0)
-        rres = self.modbus_client.execute(rreq)
+        rres = self.modbus_client.execute(False, rreq)
         dev_info["vendor_name"] = rres.information[0].decode("ascii")
         dev_info["product_code"] = rres.information[1].decode("ascii")
         dev_info["revision"] = rres.information[2].decode("ascii")
 
         rreq = ReadDeviceInformationRequest(0x2, 0)
-        rres = self.modbus_client.execute(rreq)
+        rres = self.modbus_client.execute(False, rreq)
         dev_info["vendor_url"] = rres.information[3].decode("ascii")
         dev_info["product_name"] = rres.information[4].decode("ascii")
         dev_info["model_name"] = rres.information[5].decode("ascii")
